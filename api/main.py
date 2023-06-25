e314127e5d8c36c204d8d5ac1009786491b159b5
Paddy Cooper <1210@rgsg.co.uk>
2023-06-25 20:58:20 +0100

    Added comments (docify.ai)

--- api/main.py
+++ api/main.py
@@ -12,12 +12,14 @@ load_dotenv()
 app = Flask(__name__)
 
 
-def init():
-    global answer
-    answer = False
-
-
 def get_video_id(url):
+    """
+     Extract and return the video id from a video url. This is used with the youtube_transcript_api library to get the transcript of the youtube video
+
+     @param url - The url of the video
+
+     @return The video id or None if not found in the url or the url doesn't contain the video
+    """
     match = re.search(r"=(\w+)$", url)
     if match:
         return match.group(1)
@@ -26,8 +28,18 @@ def get_video_id(url):
 
 
 def get_script(video_id, length, word_length):
+    """
+     Gets the script and max / min length. This is used to determine how long a script should be in the video
+
+     @param video_id - The YouTube video ID of the transcripts
+     @param length - The length of the script to get 1 - 4
+     @param word_length - The length of the words in the script
+
+     @return A tuple containing the script the max / min length of the script and the min / max length of
+    """
     response = YouTubeTranscriptApi.get_transcript(str(video_id))
     concatenated_text = ''
+    # Returns a string representing the response.
     for item in response:
         concatenated_text += item['text'] + ' '
     try:
@@ -36,6 +48,7 @@ def get_script(video_id, length, word_length):
         pass
 
     script = concatenated_text.strip()
+    # Calculate the minimum and maximum length of the script.
     if length == 1:
         min_length = int(len(script.split()) / 16)
         max_length = min_length + 25
@@ -57,6 +70,15 @@ def get_script(video_id, length, word_length):
 
 
 def get_summary(script, min_length, max_length):
+    """
+     Get a summary of a script. It is possible to specify min_length and max_length in the parameters
+
+     @param script - The script to run on HuggingFace
+     @param min_length - The minimum length of the script to run
+     @param max_length - The maximum length of the script to run
+
+     @return The summary of the script as a string or None if the script could not be run on Hugging
+    """
     API_KEY = os.getenv("API_KEY")
     AUTH = f"Bearer {API_KEY}"
     API_URL = "https://api-inference.huggingface.co/models/facebook/bart-large-cnn"
@@ -82,6 +104,16 @@ def get_summary(script, min_length, max_length):
 
 
 def process_video(url: str, option: int, word_length: int, check_grammar_var: bool = True):
+    """
+     Process a video URL and chosen option. This function is used to process a video URL and the option that is used to determine the video ID
+
+     @param url - URL of the video to process
+     @param option - option that is used to determine the video ID
+     @param word_length - length of the word that is used to determine the video ID
+     @param check_grammar_var - if True the grammar is checked
+
+     @return summarized video ID or None if grammar is not checked for the video ID ( default : True
+    """
     # Process the video URL and chosen option
     id = get_video_id(url=url)
 
@@ -90,6 +122,7 @@ def process_video(url: str, option: int, word_length: int, check_grammar_var: bo
     summary = get_summary(
         script=data[0], min_length=data[2], max_length=data[1])
 
+    # Check if the grammar variable is present in the script.
     if check_grammar_var == True:
         summary = check_grammar(script=summary)
 
@@ -99,6 +132,13 @@ def process_video(url: str, option: int, word_length: int, check_grammar_var: bo
 
 
 def check_grammar(script):
+    """
+     Checks the grammar of a script using the HuggingFace API.
+
+     @param script - The script to check.
+
+     @return The output of the API or None if there was an error. It's a dictionary with the following keys : generated_text : The generated
+    """
     API_KEY = os.environ.get('API_KEY')
     AUTH = f"Bearer {API_KEY}"
     API_URL = "https://api-inference.huggingface.co/models/pszemraj/flan-t5-large-grammar-synthesis"
@@ -119,6 +159,12 @@ def check_grammar(script):
 
 @app.route('/process_video', methods=['POST'])
 def process_video_route():
+    """
+     Process video and return summary. This is a REST endpoint that can be used to check if a video is valid or not.
+
+
+     @return JSON with summary of video or error if something went wrong ( not found or invalid url ). Example request **. : http Example response **
+    """
     data = request.json
     url = data.get('url')
     option = data.get('option')
@@ -130,15 +176,15 @@ def process_video_route():
     return jsonify({'summary': summary})
 
 
-@app.route('/check_grammar', methods=['POST'])
-def check_grammar_route():
-    data = request.json
-    script = data.get('script')
-    grammar_corrected_text = check_grammar(script)
-    return jsonify({'grammar_corrected_text': grammar_corrected_text})
+# @app.route('/check_grammar', methods=['POST'])
+# def check_grammar_route():
+#     data = request.json
+#     script = data.get('script')
+#     grammar_corrected_text = check_grammar(script)
+#     return jsonify({'grammar_corrected_text': grammar_corrected_text})
 
 
+# Run the Flask application.
 if __name__ == '__main__':
 
-    # Run the Flask application
     app.run()
