ff3360aa4bd1cb12b56c80b67ceeab43435075fd
Paddy Cooper <1210@rgsg.co.uk>
2023-07-04 21:42:47 +0100

    Created header + search bar

--- ytsumup-web/app/page.tsx
+++ ytsumup-web/app/page.tsx
@@ -3,7 +3,9 @@ import SearchBar from "./SearchBar";
 export default function Home() {
   return (
     <main className="bg-black">
-      <SearchBar />
+      <header className="h-[112px] bg-red-700 flex items-center justify-center">
+        <SearchBar />
+      </header>
     </main>
   );
 }
