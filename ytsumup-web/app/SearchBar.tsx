78fe8b260c16859f780b1fd3e63ed45e9d814c89
Patrick (Paddy) Cooper <80049702+PaddyCooper08@users.noreply.github.com>
2023-07-05 09:02:50 +0000

    Edited text size and made input value retrieveable

--- ytsumup-web/app/SearchBar.tsx
+++ ytsumup-web/app/SearchBar.tsx
@@ -1,18 +1,33 @@
-export default function SearchBar(props: SearchBarProps) {
+"use client";
+export default function SearchBar() {
+  function handleSubmit(e: any) {
+    // Prevent the browser from reloading the page
+    e.preventDefault();
+
+    // Read the form data
+    const form = e.target;
+    const formData = new FormData(form);
+    const searchBarContent = formData.get("Search Bar");
+  }
+
   return (
-    <div className="flex flex-wrap justify-center w-full h-full">
+    <form
+      className="flex flex-wrap justify-center w-full h-full"
+      onSubmit={handleSubmit}
+    >
       <div className="flex items-center w-1/2 ">
         <input
-          type="search"
-          className="relative bg-[#111111] pl-5 my-3 py-4 text-lg  text-left border-[#282828] rounded-l-[2.5rem] m-0 block min-w-0 flex-auto border border-solid bg-clip-padding font-normal leading-[1.6] text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-neutral-700 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none"
+          className="relative bg-[#111111] font-sans pl-5 my-3 py-4 text-xl  text-left border-[#282828] rounded-l-[2.5rem] m-0 block min-w-0 placeholder-[#7e7e7e] flex-auto border border-solid focus:border-none  leading-[1.6]  outline-none  text-white focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] "
           placeholder="Enter Youtube URL"
           aria-label="Search"
           aria-describedby="button-addon2"
+          name="Search Bar"
         />
 
-        <span
-          className="input-group-text bg-[#222222] my-3 text-lg py-[1.21rem] flex items-center whitespace-nowrap rounded-r-[2.5rem] px-8 text-center  font-normal text-neutral-700 dark:text-neutral-200"
+        <button
+          className="input-group-text bg-[#222222] my-3 text-lg py-[1.3rem] flex items-center whitespace-nowrap rounded-r-[2.5rem] px-8 text-center  font-sans "
           id="basic-addon2"
+          type="submit"
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -26,18 +41,8 @@ export default function SearchBar(props: SearchBarProps) {
               clipRule="evenodd"
             />
           </svg>
-        </span>
+        </button>
       </div>
-    </div>
+    </form>
   );
-}
-
-SearchBar.defaultProps = {
-  className: "",
-  style: {},
-};
-
-interface SearchBarProps {
-  className: string;
-  style: Object;
 }
