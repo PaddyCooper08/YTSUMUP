60a1b30325afe40ecf80252b04d339e60be12757
Paddy Cooper <1210@rgsg.co.uk>
2023-07-05 22:50:58 +0100

    Adjust for short url

--- ytsumup-web/app/Thumbnail.tsx
+++ ytsumup-web/app/Thumbnail.tsx
@@ -1,14 +1,16 @@
 export default function Thumbnail(props: any) {
   function getThumbnail() {
-    const pattern = /(?<=\?v=)[^&]+/;
-    const match = props.yturl.match(pattern);
-    if (match == null) {
-      return "Please provide a proper youtube link";
+    const pattern = /(?<=\?v=)[A-Za-z0-9]{11}/;
+    const yturl = props.yturl;
+    const match = yturl.match(pattern);
+    if (match === null) {
+      return "Please provide a proper YouTube link.";
     } else {
       const ytid = match[0];
       return `https://img.youtube.com/vi/${ytid}/maxresdefault.jpg`;
     }
   }
+
   return (
     <div>
       <img src={getThumbnail()} alt="" className="h-96" />
