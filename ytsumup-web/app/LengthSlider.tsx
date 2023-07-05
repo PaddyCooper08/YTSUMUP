9c8fc849837595c0dbb1a7b28aa3c914145a8554
Paddy Cooper <1210@rgsg.co.uk>
2023-07-05 22:50:34 +0100

    Create slider

---
+++ ytsumup-web/app/LengthSlider.tsx
@@ -0,0 +1,38 @@
+"use client";
+import { state } from "./state";
+import Slider from "@mui/material/Slider";
+import { snapshot, useSnapshot } from "valtio";
+
+export default function LengthSlider() {
+  const handleChange = (event: Event, newValue: number | number[]) => {
+    let sliderValue = newValue as number;
+
+    // @ts-ignore
+    state.sliderValue = sliderValue;
+  };
+  const snap = useSnapshot(state);
+  const sliderValue: number = snap.sliderValue;
+  return (
+    <div className="p-8 text-roboto ">
+      <h1 className="text-4xl text-[#b9b9b9] text-center p-2  ">
+        Summary Length
+      </h1>
+      <h1 className="text-sm text-[#b9b9b9] text-center p-1  ">
+        as a percentage of transcript length
+      </h1>
+      <h1 className="text-2xl text-[#b9b9b9] text-center p-2 ">
+        {sliderValue}%
+      </h1>
+      <Slider
+        defaultValue={50}
+        aria-label="Default"
+        valueLabelDisplay="off"
+        step={10}
+        sx={{
+          color: "#3d3d3d",
+        }}
+        onChange={handleChange}
+      />
+    </div>
+  );
+}
