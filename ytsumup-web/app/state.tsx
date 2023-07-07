c824bc72a3a9c4e1e52b7dcf9e01e7404a7f3a67
Paddy Cooper <1210@rgsg.co.uk>
2023-07-07 16:44:26 +0100

    Added ytid state

--- ytsumup-web/app/state.tsx
+++ ytsumup-web/app/state.tsx
@@ -4,5 +4,6 @@ const state = proxy({
   sliderValue: 50,
   thumbnailUrl:
     "https://raw.githubusercontent.com/PaddyCooper08/YTSUMUP/677bb02751501994c3ea8297b35bc3f7d1e43326/ytsumup-web/assets/enterURL.svg",
+  ytid: "",
 });
 export { state };
