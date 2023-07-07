61296277524691b51cbd7f0ec5e1a5a5ddd11b98
Paddy Cooper <1210@rgsg.co.uk>
2023-07-07 16:56:29 +0100

    Swap img tag for next/Image

--- ytsumup-web/app/Thumbnail.tsx
+++ ytsumup-web/app/Thumbnail.tsx
@@ -2,6 +2,7 @@
 import { state } from "./state";
 import YoutubeTitle from "./YoutubeTitle";
 import { Suspense } from "react";
+import Image from "next/image";
 
 import { snapshot, useSnapshot } from "valtio";
 
@@ -11,10 +12,12 @@ export default function Thumbnail(props: any) {
   if (snap.ytid == "") {
     return (
       <div>
-        <img
+        <Image
           src={snap.thumbnailUrl}
-          alt=""
-          className="h-96  border-solid border-4 border-[#222222] font-sans"
+          width="1000"
+          height="500"
+          alt="Youtube Thumbnail"
+          className="  border-solid border-4 border-[#222222] font-sans"
         />
         <h1>{state.ytid}</h1>
       </div>
