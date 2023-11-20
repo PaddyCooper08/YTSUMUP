1e54486a9bafad7c3473f237ba955996120cc8e8
Paddy Cooper <1210@rgsg.co.uk>
2023-11-20 19:16:22 +0000

    adsense verification

--- ytsumup-web/app/layout.tsx
+++ ytsumup-web/app/layout.tsx
@@ -15,6 +15,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3180471462789849"
+        ></script>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
